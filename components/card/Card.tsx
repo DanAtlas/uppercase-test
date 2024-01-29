3914892b41a0e4561fc3d6825f8629d7b7a91d38
Dias <abishev295@gmail.com>
2024-01-29 20:00:19 +0600

    some package and architechture updates

---
+++ components/card/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Card.module.scss';
+
+const Card = () => {
+  return (
+    <div className={styles['card']}>
+      <p>card text</p>
+    </div>
+  );
+}
+
+export default Card;
